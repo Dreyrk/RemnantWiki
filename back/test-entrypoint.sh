ba3164b7299931f622141f62d3f04d5b5041cf0f
Dreyrk <lucas.rondepierre123@gmail.com>
2023-06-29 14:31:14 +0200

    fix ci

--- back/test-entrypoint.sh
+++ back/test-entrypoint.sh
@@ -1,7 +1,9 @@
 #bin/bash
 
 echo "\n###\n# Launch API...\n###"
-nohup npm start
+nohup npm start &
+echo "\n###\n# Populate DB with seed...\n###"
+node ./seed.js
 echo "\n###\n# Launch Units Tests...\n###"
 npm run mocha &
 PID2=$!
